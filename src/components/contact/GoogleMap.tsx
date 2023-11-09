063386fd6b3e39e8c7823ffa03683291072a88ac
bencxiao <lighthouseconsultingca@gmail.com>
2023-11-08 23:45:15 -0800

    Fix google map

--- src/components/contact/GoogleMap.tsx
+++ src/components/contact/GoogleMap.tsx
@@ -6,9 +6,7 @@ const GoogleMap = () => {
               <section className="map-area">
                     <div id="contact-map" className="contact-page-map">
                         <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2506.0989510101945!2d-114.03219422240247!3d51.08818134124079!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5371650479d7489b%3A0x9d5be6445fcd8b5c!2s1431%2040%20Ave%20NE%20%239%20Calgary%2C%20AB%20T2E%208N6!5e0!3m2!1sen!2sca!4v1699340929387!5m2!1sen!2sca"
-                            width="800" height="600" allowFullScreen="" loading="lazy"
-                            referrerPolicy="no-referrer-when-downgrade"></iframe>
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2506.0989510101945!2d-114.03219422240247!3d51.08818134124079!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5371650479d7489b%3A0x9d5be6445fcd8b5c!2s1431%2040%20Ave%20NE%20%239%20Calgary%2C%20AB%20T2E%208N6!5e0!3m2!1sen!2sca!4v1699340929387!5m2!1sen!2sca"/>
                     </div>
                 </section>
         </>
